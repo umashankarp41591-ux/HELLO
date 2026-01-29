74715c9b6c3372973e21e19ab6b109f85092d0f5
umashankarp41591-ux <umashankarp41591@gmail.com>
2026-01-29 15:50:38 +0530

    Add files via upload

---
+++ code7.java
@@ -0,0 +1,15 @@
+class code7{
+    public static void main(String[]args){
+        int a=10;
+        int b=4;
+        if(a>b)
+        {
+        System.out.println(a);
+
+        }
+        else{
+            System.out.println(b);
+        
+    }
+    }
+    }
