cc88e49f56edfa623de932f1bdf7bc5f5d244d15
umashankarp41591-ux <umashankarp41591@gmail.com>
2026-01-30 10:22:01 +0530

    Add files via upload

---
+++ code8.java
@@ -0,0 +1,9 @@
+class code8{
+    public static void main(String[]args){
+        int p=10;
+        int r=5;
+        double t=9.00;
+        int s1=(10*5*9)/100;
+        System.out.println(s1);
+    }
+}
